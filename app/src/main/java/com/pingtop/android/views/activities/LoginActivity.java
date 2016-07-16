e160161b843ae18294b3ad11796a34d880934817
wuhaojie <w19961009@126.com>
2016-07-16 12:14:48 +0800

    add login.

---
+++ app/src/main/java/com/pingtop/android/views/activities/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.pingtop.android.views.activities;
+
+import android.os.Bundle;
+
+import com.pingtop.android.R;
+import com.pingtop.android.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    public void initializeInjector() {
+
+    }
+
+    @Override
+    public int getLayoutResID() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+}
