7dae5cded81648b3dd7b991ec679696773263ced
wuhaojie <w19961009@126.com>
2016-08-06 23:51:47 +0800

    update ui.

--- app/src/main/java/com/pingtop/android/views/fragments/main/CommunityFragment.java
+++ app/src/main/java/com/pingtop/android/views/fragments/main/CommunityFragment.java
@@ -1,19 +1,28 @@
 package com.pingtop.android.views.fragments.main;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pingtop.android.R;
+import com.pingtop.android.views.activities.WriteMessageActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CommunityFragment extends Fragment {
+public class CommunityFragment extends Fragment implements View.OnClickListener {
 
+
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
 
     public CommunityFragment() {
         // Required empty public constructor
@@ -24,7 +33,19 @@ public class CommunityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false);
+        View view = inflater.inflate(R.layout.fragment_community, container, false);
+        ButterKnife.bind(this, view);
+        mFab.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                Intent intent = new Intent(getActivity(), WriteMessageActivity.class);
+                getActivity().startActivity(intent);
+                break;
+        }
+    }
 }
